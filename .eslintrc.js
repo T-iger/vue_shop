8fdde43dff43715500d9a43f72d9fb3b2db71520
T-iger <885515333@qq.com>
2020-09-08 15:47:45 +0800

    完成登录功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
+    //去除缩进检查
+    'indent': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
+// 导入axios
+import axios from 'axios'
 
+// 配置请求路径
+axios.defaults.baseURL = 'http://127.0.0.1:9999'
 Vue.config.productionTip = false
+// 全局配置$http，用来发送
+Vue.prototype.$http = axios
 
 new Vue({
   router,
